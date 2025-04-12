173cf757f23e3bc5534b315b9744f03dfefaea12
Chris <chrisbreuer93@gmail.com>
2025-04-11 18:03:12 -0700

    chore: wip

--- bin/cli.ts
+++ bin/cli.ts
@@ -4,26 +4,7 @@ import path from 'node:path'
 import process from 'node:process'
 import { CAC } from 'cac'
 import { version } from '../package.json'
-
-// Create a default config in case the import fails
-const defaultConfig = {
-  verbose: false,
-  rules: {
-    'conventional-commits': 2,
-    'header-max-length': [2, { maxLength: 72 }],
-    'body-max-line-length': [2, { maxLength: 100 }],
-    'body-leading-blank': 2,
-    'no-trailing-whitespace': 1,
-  },
-  defaultIgnores: [
-    '^Merge branch',
-    '^Merge pull request',
-    '^Merged PR',
-    '^Revert ',
-    '^Release ',
-  ],
-  ignores: [],
-}
+import { defaultConfig } from '../src/config'
 
 const cli = new CAC('gitlint')
 
